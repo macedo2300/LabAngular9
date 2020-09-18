8ad63b38b41835d95a2f8fedc286b786e81412a0
Vagner <macedo2300@2gmail.com>
2020-09-18 16:17:08 -0300

    Adicionando o back-end ao projeto.

---
+++ pibape-back/src/main/java/com/br/crud/pibape/PibapeBackApplication.java
@@ -0,0 +1,13 @@
+package com.br.crud.pibape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibapeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibapeBackApplication.class, args);
+	}
+
+}
