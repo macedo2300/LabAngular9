02811805b85aa63a66a925bcc7b849bbef888708
Vagner <macedo2300@2gmail.com>
2020-09-18 16:17:08 -0300

    Adicionando o bac-end ao projeto.

---
+++ pibape-back/src/test/java/com/br/crud/pibape/PibapeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.crud.pibape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PibapeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
